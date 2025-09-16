fe91029d08724741ca85e122aeb8f95e7c043178
ricardoV94 <ricardo.vieira1994@gmail.com>
2025-09-16 14:29:57 +0200

    Speedup OrderedSet

--- pytensor/misc/ordered_set.py
+++ pytensor/misc/ordered_set.py
@@ -1,9 +1,16 @@
 from collections.abc import Iterable, Iterator, MutableSet
-from typing import Any
+from typing import Any, Literal
 
 
 class OrderedSet(MutableSet):
-    values: dict[Any, None]
+    """Ordered-set like data structure.
+
+    Uses a dictionary with None values to store the set elements.
+    Relying on the fact that Python dicts maintain insertion order.
+    """
+
+    __slots__ = ("values",)
+    values: dict[Any, Literal[None]]
 
     def __init__(self, iterable: Iterable | None = None) -> None:
         if iterable is None:
@@ -24,21 +31,28 @@ class OrderedSet(MutableSet):
         self.values[value] = None
 
     def discard(self, value) -> None:
-        if value in self.values:
+        try:
             del self.values[value]
+        except KeyError:
+            pass
 
     def copy(self) -> "OrderedSet":
-        return OrderedSet(self)
+        new_set = OrderedSet()
+        new_set.values = self.values.copy()
+        return new_set
 
     def update(self, other: Iterable) -> None:
-        for value in other:
-            self.add(value)
+        self.values.update({o: None for o in other})
 
     def union(self, other: Iterable) -> "OrderedSet":
-        new_set = self.copy()
-        new_set.update(other)
+        new_set = OrderedSet()
+        new_set.values = self.values.copy() | {o: None for o in other}
         return new_set
 
     def difference_update(self, other: Iterable) -> None:
+        self_values = self.values
         for value in other:
-            self.discard(value)
+            try:
+                del self_values[value]
+            except KeyError:
+                pass
